56df20cf37f8c5a6b9f0f96e095a14e1a8517430
Scott Sanders <jssjr@github.com>
2017-02-18 11:01:33 -0500

    Implement the basic structure of our Sensors class

---
+++ src/org/usfirst/frc/team4533/robot/utils/Sensors.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team4533.robot.utils;
+
+public class Sensors {
+
+	// Sensor readings
+	public static double heading;
+	public static double rearDistance;
+	public static String vision;
+	
+	// Protocol delimiters
+	private static final byte syncByte = 94;
+	private static final byte endSec = 126;
+	
+	public void Sensors() {
+		// We should probably set some things up here...
+	}
+	
+	public void fetchData() {
+		// Read information from the arduino serial if we can, then update the sensor values we're tracking
+	}
+}
